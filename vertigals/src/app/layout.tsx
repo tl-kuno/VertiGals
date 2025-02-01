8789a0c5702b41bcfd1aeab3c7a35078aa587dfc
tl-kuno <taylor.l.kuno@gmail.com>
2025-02-01 13:49:45 -0600

    Site info

--- vertigals/src/app/layout.tsx
+++ vertigals/src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next';
 import '../app/css/main.css';
 
 export const metadata: Metadata = {
-    title: 'North Shore VertiGals',
-    description: 'A community of women-identifying climbers in Duluth, MN'
+  title: "North Shore VertiGals",
+  description: "A community of women-identifying climbers in Duluth, MN",
 };
 
 export default function RootLayout({
