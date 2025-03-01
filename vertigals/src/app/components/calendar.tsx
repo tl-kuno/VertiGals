4545003a6de048a7d8a55e2c6cdd8e07000b229a
tl-kuno <taylor.l.kuno@gmail.com>
2025-03-01 11:05:58 -0600

    update linter settings

--- vertigals/src/app/components/calendar.tsx
+++ vertigals/src/app/components/calendar.tsx
@@ -1,8 +1,5 @@
 export default function GoogleCalendar() {
-    return (
-        <iframe
-            className="calendar"
-            src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FChicago&showPrint=0&mode=AGENDA&showTz=0&showCalendars=0&showTabs=0&title&showTitle=0&src=OXZ0bjZjcjU0ZDljZ3A5cmo0aG5mOGtscmtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23C0CA33"
-        ></iframe>
-    );
+  return (
+    <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FChicago&showPrint=0&mode=AGENDA&showTz=0&showCalendars=0&showTabs=0&title&showTitle=0&src=OXZ0bjZjcjU0ZDljZ3A5cmo0aG5mOGtscmtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23C0CA33" style={{borderWidth:0}} width="800" height="600" frameBorder="0" scrolling="no"></iframe>
+  );
 }
