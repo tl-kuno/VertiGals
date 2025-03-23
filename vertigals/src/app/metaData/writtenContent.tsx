616f564fc002e73419db2d6425e7b4be07df12d8
tl-kuno <taylor.l.kuno@gmail.com>
2025-03-23 16:13:36 -0500

    calendar embedded and some interactivity

---
+++ vertigals/src/app/metaData/writtenContent.tsx
@@ -0,0 +1,6 @@
+export const writtenContent = {
+    home: {
+        whoAreWe: `We are committed to strengthening our climbing community through fostering diversity and supporting women-identifying and non-binary climbers of all ages and skill-levels by creating a safe space to climb, learn, and grow.`,
+        whatWeDo: `We host regular climb nights at local gyms and crags, organize climbing skills clinics, and hold social events to provide opportunities for building relationships in the climbing community.`
+    }
+};
--- vertigals/src/app/pages/home.tsx
+++ vertigals/src/app/pages/home.tsx
@@ -1,15 +1,15 @@
 import SectionCards from '../components/sectionCards';
-import NavigationProps from '../metaData/propsInterface';
+import { NavigationProps } from '../metaData/propsInterface';
+import { writtenContent } from '../metaData/writtenContent';
 
 export default function Home(props: NavigationProps) {
-    const bodyText =
-        'Supporting information about NSVG Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.';
-
     const onClick = () => {
         const mailtoLink =
             'mailto:northshorevertigals@email.com?subject=Email List&body=Hello, Please add me to your email list!';
         window.location.href = mailtoLink;
     };
+
+    const aboutText = `${writtenContent.home.whoAreWe} ${writtenContent.home.whatWeDo}`;
 
     return (
         <>
@@ -21,7 +21,7 @@ export default function Home(props: NavigationProps) {
                     A Women&apos;s Climbing Community on the North Shore of Lake
                     Superior
                 </h1>
-                <p className="copy-primary">{bodyText}</p>
+                <p className="copy-primary">{aboutText}</p>
                 <div className="grid-container halves u-vw80">
                     <SectionCards {...props} />
                 </div>
@@ -29,7 +29,7 @@ export default function Home(props: NavigationProps) {
                     Want to stay up-to-date with the North Shore VertiGals?
                 </h2>
                 <div className="flex flex-row fr-center">
-                    <p className="copy-primary">
+                    <p className="copy-accent">
                         Join our email list to get the latest news!
                     </p>
                     <button className="button-primary" onClick={onClick}>
