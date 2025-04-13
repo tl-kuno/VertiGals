6c4ab7d20a6e4978e4d4311c903309c0f4a0030c
tl-kuno <taylor.l.kuno@gmail.com>
2025-04-13 17:48:07 -0500

    file restructure commit

---
+++ src/app/metaData/imageMap.tsx
@@ -0,0 +1,35 @@
+import { StaticImageData } from 'next/image';
+
+// HOME
+import home_hero from '../images/home_banner.png';
+
+// ABOUT
+import about_banner from '../images/about_banner.png';
+import andrea from '../images/board/andrea.jpg';
+import brooke from '../images/board/brooke.jpg';
+import claire from '../images/board/claire.jpg';
+import colleen from '../images/board/colleen.jpg';
+import kathryn from '../images/board/kathryn.jpg';
+import marta from '../images/board/marta.png';
+import taylor from '../images/board/taylor.jpg';
+
+interface ImageMap {
+    [key: string]: {
+        image: StaticImageData;
+        alt: string;
+    };
+}
+
+export const ImageMap: ImageMap = {
+    // HOME
+    homeHero: { image: home_hero, alt: 'Climbing above icy Lake Superior' },
+    // ABOUT
+    aboutHero: { image: about_banner, alt: 'Climber looking at Lake Superior' },
+    andrea: { image: andrea, alt: 'Andrea' },
+    brooke: { image: brooke, alt: 'Brooke' },
+    claire: { image: claire, alt: 'Claire' },
+    colleen: { image: colleen, alt: 'Colleen' },
+    kathryn: { image: kathryn, alt: 'Kathryn' },
+    marta: { image: marta, alt: 'Marta' },
+    taylor: { image: taylor, alt: 'Taylor' }
+};
---
+++ src/app/metaData/writtenContent.tsx
@@ -0,0 +1,46 @@
+export const writtenContent = {
+    home: {
+        whoAreWe: `We are committed to strengthening our climbing community through fostering diversity and supporting women-identifying and non-binary climbers of all ages and skill-levels by creating a safe space to climb, learn, and grow.`,
+        whatWeDo: `We host regular climb nights at local gyms and crags, organize climbing skills clinics, and hold social events to provide opportunities for building relationships in the climbing community.`
+    },
+    about: {
+        history: [
+            `The Vertigals were founded in 2018 by Erin Hammes and Molly Wick with the goal of fostering an inclusive space for women climbers in the Twin Ports region. The founders wanted to give women the opportunity to learn from and climb with other women at local gyms and crags while giving back to the local climbing community by promoting climbing safety and diverse climbing perspectives.`,
+            `Since then, the Vertigals have organized many community events, including Climb Nights at the local climbing gyms, outdoor climbing clinics, and social gatherings at board member’s homes. They also partner with local businesses to put on their signature fundraiser, the Nuts of Steel Rock Climbing Film Festival, which brings climbers together for a night of relaxation, raffle prizes, and tasty food and drink.`,
+            `The NorthShore Vertigals are also committed to encouraging women in the outdoors on and off the wall and are often found at local events across the Twin Ports. Come say “Hi” when you see  us wearing our merch out and about!`
+        ],
+        boardMembers: [
+            {
+                name: 'Taylor',
+                position: 'President',
+                bio: `Taylor tried climbing for the first time shortly after graduating college and was immediately tied-in (pun intended). She boasts a love-hate relationship with climbing’s type-II fun nature. Against her better judgement she still somehow regularly finds herself hanging mid-air off the side of a cliff.`
+            },
+            {
+                name: 'Kathryn',
+                position: 'Treasurer',
+                bio: `A Minnesota native, Kathryn started climbing in graduate school while attending Texas A&M University. While Kathryn's more of a roped climbing person, here she is getting a bouldering session in before defending her doctoral dissertation! Now a professor at UMD, Kathryn enjoys gym climbing and spends her summers running and mountain biking on the many trails in Duluth.`
+            },
+            {
+                name: 'Andrea',
+                position: 'Secretary',
+                bio: `Andrea likes climbing because  it’s like a fun little puzzle that everyone can solve in their own way. Her favorite is sport climbing, or anything that gives an excuse to travel somewhere new! When not climbing, you can usually find her reading a fantasy book, bike riding, or planning a side quest to another country.`
+            },
+            {
+                name: 'Brooke',
+                bio: `Brooke started her climbing journey 7 years ago while she was attending UMD. She enjoys climbing because it pushes her to reach for new heights both physically and mentally. Another component she loves about climbing is the amazing community it comes with. Currently, Brooke holds a board position on the North Shore Vertigals and the Duluth Climbers Coalition. She is excited to keep growing in her climbing journey and make many more connections.`
+            },
+            {
+                name: 'Colleen',
+                bio: `Colleen got her start with climbing while teaching outdoor education. She joined the VertiGals after moving to Duluth, and has enjoyed building community with other female climbers. When not climbing you can find her hiking, paddling and drinking coffee anywhere along the shore.`
+            },
+            {
+                name: 'Claire',
+                bio: `My name is Claire and I haven't given Taylor my bio yet.`
+            },
+            {
+                name: 'Marta',
+                bio: `Marta is a climber and avid traveler who calls Duluth her homebase. With incurable wanderlust and a never ending desire to be outdoors, she can often be found plotting her next western road trip, hiking, climbing, XC skiing, going to a cabin or enjoying her sauna. She responds well to mountains and cheese.`
+            }
+        ]
+    }
+};
